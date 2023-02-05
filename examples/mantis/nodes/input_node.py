4bdb9859bf41dd4c2569dc3576a3505ea4bb2d4f
ZikriBen <zikri.ben@gmail.com>
2023-02-05 12:24:41 +0200

    initial mantis branch

--- examples/mantis/nodes/input_node.py
+++ examples/mantis/nodes/input_node.py
@@ -91,3 +91,33 @@ class CalcNode_S_MVX_File(CalcNode):
             layout.addWidget(t)
 
         return self.colaps_widget
+
+
+@register_nodes(OP_NODE_S_UUID)
+class CalcNode_S_MVX_File(CalcNode):
+    icon = str(current_file_path.joinpath(r"icons\in.png"))
+    op_code = OP_NODE_S_UUID
+    op_title = "UUID"
+    content_label_obj_name = "mvx_source_node_bg"
+    colaps_widget = None
+
+    def __init__(self, scene) -> None:
+        super().__init__(scene, inputs=[], outputs=[3])
+
+    def createParamWidget(self):
+        if not self.colaps_widget:
+            self.colaps_widget = QWidget()
+            self.colaps_widget.setMinimumWidth(250)
+            self.colaps_widget.setStyleSheet("")
+            self.colaps_widget.setObjectName(str(self.id))
+            layout = QVBoxLayout()
+            layout.setSpacing(0)
+            layout.setAlignment(Qt.AlignTop)
+            self.colaps_widget.setLayout(layout)
+
+            t = FrameLayout(title=self.__class__.__name__)
+            t.addWidget(QPushButton('test'))
+            t.addWidget(QLineEdit())
+            layout.addWidget(t)
+
+        return self.colaps_widget
